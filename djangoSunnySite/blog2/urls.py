6e2bd42d6b04d3089513c6a65b518d47b9ab81ad
SunnyBingoMe <SunnyBingoMe@gmail.com>
2016-09-03 20:51:36 +0200

    update url, finishing with slash

--- djangoSunnySite/blog2/urls.py
+++ djangoSunnySite/blog2/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>\d+)$', views.DetailView.as_view(), name='detail'),
-    url(r'^post/create$', views.PostCreate.as_view(), name='post-create'),
+    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^edit/create/$', views.PostCreate.as_view(), name='post-create'),
 ]
