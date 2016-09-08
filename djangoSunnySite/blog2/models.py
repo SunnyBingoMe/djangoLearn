3ad37c62985761e12d392346ebcfeb089a946968
SunnyBingoMe <SunnyBingoMe@gmail.com>
2016-09-08 21:03:59 +0200

    django-baker: before baking

--- djangoSunnySite/blog2/models.py
+++ djangoSunnySite/blog2/models.py
@@ -12,3 +12,18 @@ class Post(models.Model): # this will create a table "blog_post" in small letter
     
     def __str__(self):
         return self.title
+
+
+class Media(models.Model):
+    IMAGE = 'image'
+    YOUTUBE = 'youtube'
+    AUDIO = 'audio'
+    type_choice_s = (
+        (IMAGE, 'Image'),
+        (YOUTUBE, 'YouTube'),
+        (AUDIO, 'Audio'),
+    )
+
+    announce_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    type = models.CharField(max_length=140, choices=type_choice_s, default=IMAGE)
+    filename = models.CharField(max_length=140, default='20160904/1.jpg')
